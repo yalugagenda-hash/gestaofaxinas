6162719013cfe3000fd59cf663bda43cd830d4dc
yalugagenda-hash <yalug.agenda@gmail.com>
2026-07-22 09:31:05 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         brand: {
           50: '#eff6ff',
           100: '#dbeafe',
-          500: '#3b82f6',
+          500: '#ff5b6c',
           600: '#2563eb',
           700: '#1d4ed8',
         },
